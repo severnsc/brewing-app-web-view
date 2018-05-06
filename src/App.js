9ed53bedd6bba3342b7690075853d20bd69a7ab8
severnsc <severnsc@gmail.com>
2018-05-06 17:34:51 -0400

    Implement main routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SignupForm from './components/signupForm'
+import { BrowserRouter } from 'react-router-dom'
+import Main from './Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <SignupForm navigate={() => console.log("navigated!")} />
+        <Main />
       </div>
     );
   }
 }
 
-export default App;
+const AppWithRouting = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+
+export default AppWithRouting;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppWithRouting from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppWithRouting />, document.getElementById('root'));
 registerServiceWorker();
