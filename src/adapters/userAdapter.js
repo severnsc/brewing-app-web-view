9c1250b98fee70d393a81fe77c7e75b558624123
severnsc <severnsc@gmail.com>
2018-05-31 17:19:04 -0400

    Move createUser to userAdapter

--- src/adapters/userAdapter.js
+++ src/adapters/userAdapter.js
@@ -5,3 +5,13 @@ export const isUsernameUnique = async username => {
     username: username
   }).then(response => response.data)
 }
+
+export const createUser = async (username, password, email) => {
+  return axios.post('http://localhost:3001/signup', {
+    username,
+    password,
+    email
+  })
+    .then(response => response)
+    .catch(e => e)
+}
--- src/auth.js
+++ src/auth.js
@@ -1,15 +1,5 @@
 import axios from 'axios'
 
-export const createUser = async (username, password, email) => {
-  return axios.post('http://localhost:3001/signup', {
-    username,
-    password,
-    email
-  })
-    .then(response => response)
-    .catch(e => e)
-}
-
 export const loginUser = async (username, password) => {
   return axios.post('http://localhost:3001/login', {
     username,
