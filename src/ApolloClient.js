f3c77c45c14b0bda7d7ce7f32177bd899270b06a
Christopher Severns <chris@g2i.co>
2018-07-02 10:50:46 -0400

    Add errorLink to ApolloClient

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -3,6 +3,7 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloLink } from 'apollo-link'
 import { withClientState } from 'apollo-link-state'
+import { onError } from "apollo-link-error";
 import resolvers from './resolvers'
 import defaultState from "./defaultState"
 
@@ -23,10 +24,22 @@ const stateLink = withClientState({
 
 const httpLink = new HttpLink({uri,credentials: "include"})
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
 const client = new ApolloClient({
   cache,
   link: ApolloLink.from([
     stateLink,
+    errorLink,
     httpLink
   ])
 })
