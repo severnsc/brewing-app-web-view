a464bb369ce681fd8ce9a4e189868a64c77acf22
Christopher Severns <chris@g2i.co>
2018-07-10 22:27:01 -0400

    return data from updateInventoryItem mutation in order to update the view

--- src/mutations/inventoryItem.js
+++ src/mutations/inventoryItem.js
@@ -4,6 +4,19 @@ export default gql`
   mutation updateInventoryItem($id: String!, $inventoryId: String!, $object: String!, $costUnit: String!, $unitCost: Float!, $reorderCost: Float!, $quantityUnit: String!, $currentQuantity: Float!, $reorderQuantity: Float!, $reorderThreshold: Float!, $lastReorderDate: String!, $deliveryDate: String!) {
     updateInventoryItem(id: $id, inventoryId: $inventoryId, object: $object, costUnit: $costUnit, unitCost: $unitCost, reorderCost: $reorderCost, quantityUnit: $quantityUnit, currentQuantity: $currentQuantity, reorderQuantity: $reorderQuantity, reorderThreshold: $reorderThreshold, lastReorderDate: $lastReorderDate, deliveryDate: $deliveryDate) {
       id
+			inventory {
+				id
+			}
+			object
+			costUnit
+			unitCost
+			reorderCost
+			quantityUnit
+			currentQuantity
+			reorderQuantity
+			reorderThreshold
+			lastReorderDate
+			deliveryDate
     }
   }
 `
