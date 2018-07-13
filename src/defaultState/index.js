d2dacbad73933924275ad558429b3890fad980db
Christopher Severns <chris@g2i.co>
2018-07-12 21:33:17 -0700

    Rename timers to timersTable in defaultState

--- src/defaultState/index.js
+++ src/defaultState/index.js
@@ -2,7 +2,7 @@ import dashboard from "./dashboard"
 import login from "./login"
 import signup from "./signup"
 import modal from "./modal"
-import timersTable from "./timers"
+import timersTable from "./timersTable"
 import activeTimer from "./activeTimer"
 
 export default {
