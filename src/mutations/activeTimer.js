db8935b6c01cb7108ae064a5f93b7a30dbedda13
Christopher Severns <chris@g2i.co>
2018-08-27 10:19:07 -0400

    return id from activeTimer mutation

--- src/mutations/activeTimer.js
+++ src/mutations/activeTimer.js
@@ -2,6 +2,8 @@ import gql from "graphql-tag"
 
 export default gql`
   mutation updateActiveTimer($id: String!) {
-    updateActiveTimer(id: $id) @client
+    updateActiveTimer(id: $id) @client {
+    	id
+    }
   }
 `
