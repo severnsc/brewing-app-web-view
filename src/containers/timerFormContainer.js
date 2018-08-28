4b044f948f3358fda7587f9c8366c3d9951f8264
Christopher Severns <chris@g2i.co>
2018-08-28 13:20:41 -0400

    Add ability to delete timer alerts

--- src/containers/timerFormContainer.js
+++ src/containers/timerFormContainer.js
@@ -5,7 +5,8 @@ import {
 	CREATE_TIMER_ALERT,
 	UPDATE_ACTIVE_TIMER,
 	UPDATE_TIMER,
-	UPDATE_TIMER_ALERT
+	UPDATE_TIMER_ALERT,
+	DELETE_TIMER_ALERT
 } from "../mutations"
 import { TimerForm } from "../components"
 import PropTypes from "prop-types"
@@ -63,18 +64,47 @@ const TimerFormContainer = ({ id }) => (
 															}
 
 															return(
-																<Fragment>
-																	<p>All times are in HH:MM:SS format</p>
-																	<button onClick={activateTimer}>Activate timer</button>
-																	<TimerForm
-																		name={timer.name}
-																		duration={timer.duration}
-																		timerAlerts={timer.timerAlerts}
-																		saveTimer={saveTimer}
-																		saveTimerAlert={saveTimerAlert}
-																		addTimerAlert={createTimerAlert}
-																	/>
-																</Fragment>
+																<Mutation
+																	mutation={DELETE_TIMER_ALERT}
+																	update={(cache, { data: { deleteTimerAlert }}) => {
+																		const { currentUser } = cache.readQuery({ query: timersQuery })
+																		const { timers } = currentUser
+																		const timer = timers.find(timer => timer.id === id)
+																		const { timerAlerts } = timer
+																		const newTimerAlerts = timerAlerts.filter(alert => alert.id !== deleteTimerAlert.id)
+																		const data = {
+																			currentUser: {
+																				...currentUser,
+																				timers: timers.map(timer => timer.id === id ? {...timer, timerAlerts: newTimerAlerts} : timer)
+																			}
+																		}
+																		cache.writeQuery({ query: timersQuery, data })
+																	}}
+																>
+																	{deleteTimerAlertMutation => {
+
+																		const deleteTimerAlert = id => {
+																			deleteTimerAlertMutation({ variables: { id } })
+																		}
+
+																		return(
+																			<Fragment>
+																				<p>All times are in HH:MM:SS format</p>
+																				<button onClick={activateTimer}>Activate timer</button>
+																				<TimerForm
+																					name={timer.name}
+																					duration={timer.duration}
+																					timerAlerts={timer.timerAlerts}
+																					saveTimer={saveTimer}
+																					saveTimerAlert={saveTimerAlert}
+																					addTimerAlert={createTimerAlert}
+																					deleteTimerAlert={deleteTimerAlert}
+																				/>
+																			</Fragment>
+																		)
+
+																	}}
+																</Mutation>
 															)
 
 														}}
