b3d52b596e329f39d3cd4d283fa6a1c95d4e5b7f
Christopher Severns <chris@g2i.co>
2018-09-07 13:26:24 -0400

    Style signup screen

--- src/components/Forms/SignupForm/styles.js
+++ src/components/Forms/SignupForm/styles.js
@@ -1,9 +1,14 @@
 import { green } from "../../constants"
 
 export default {
-	flexColumn: {
-    display: "flex",
-    flexDirection:"column"
+  container: {
+    background: "hsl(0,0%,100%)",
+    padding: "50px",
+    borderRadius: "20px",
+    boxShadow: "0 4px 6px 0 hsla(0, 0%, 0%, 0.2)"
+  },
+	form: {
+    marginTop: "20px"
   },
   login: {
   	fontSize: "0.8em",
