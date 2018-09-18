77bc91d9f283fdf23c452c2a814d654738020dd8
Christopher Severns <chris@g2i.co>
2018-09-18 15:42:35 -0400

    Remove reference to FixedNewItemButton

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,6 @@ import ResetPasswordForm from "./ResetPasswordForm"
 import SearchBar from "./SearchBar"
 import SortableTable from "./SortableTable"
 import InventoryForm from "./InventoryForm"
-import FixedNewItemButton from "./FixedNewItemButton"
 import NewTimerForm from "./NewTimerForm"
 import NewTimerAlertForm from "./NewTimerAlertForm"
 import Tabs from "./Tabs"
@@ -35,7 +34,6 @@ export {
 	TimerAlertForm,
 	TimerForm,
 	InventoryForm,
-	FixedNewItemButton,
 	NewTimerForm,
 	NewTimerAlertForm,
 	Tab,
