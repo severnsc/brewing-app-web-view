296ec7c41cf3f67b7f53aa01254162bcc000d9bf
Christopher Severns <chris@g2i.co>
2018-09-26 17:04:28 -0400

    Replace the sortable table with a scrollable list in the needs to be reordered container

--- src/containers/needsToBeReorderedContainer.js
+++ src/containers/needsToBeReorderedContainer.js
@@ -1,13 +1,7 @@
 import React from "react"
 import { Query } from "react-apollo"
 import { needsToBeReorderedTableQuery } from "../queries"
-import {
-	UPDATE_NEEDS_TO_BE_REORDERED_TABLE_SORT,
-	UPDATE_NEEDS_TO_BE_REORDERED_TABLE_ITEM_LIMIT,
-	UPDATE_NEEDS_TO_BE_REORDERED_TABLE_PAGE_NUMBER,
-	UPDATE_MODAL
-} from "../mutations"
-import SortableTableContainer from "./common/sortableTableContainer"
+import { ScrollableList } from "../components"
 import shortid from "shortid"
 
 const NeedsToBeReorderedContainer = () => (
@@ -20,51 +14,34 @@ const NeedsToBeReorderedContainer = () => (
 				return <p>Error!</p>
 			}
 
-			const columns = [
-				{id: shortid.generate(), name: "Item name"},
-				{id: shortid.generate(), name: "Inventory name"},
-				{id: shortid.generate(), name: "Amount"},
-				{id: shortid.generate(), name: "Reorder quantity"},
-				{id: shortid.generate(), name: "Unit cost"},
-				{id: shortid.generate(), name: "Reorder cost"}
-			]
-
 			const { currentUser } = data
 			const items = currentUser.inventories.map(inventory => inventory.items).flat()
 			const itemsToReorder = items.filter(item => item.currentQuantity <= item.reorderThreshold)
-			const tableRows = itemsToReorder.map(item => ({
-				id: item.id,
-				cells: [
-					JSON.parse(item.object).name,
-					currentUser.inventories.find(inv => inv.id === item.inventory.id).name,
-					item.currentQuantity,
-					item.reorderQuantity,
-					item.unitCost,
-					item.reorderCost
-				]
+			const dataItems = itemsToReorder.map(item => ({
+				key: shortid.generate(),
+				name: JSON.parse(item.object).name,
+				amount: item.currentQuantity
 			}))
 
-			const {
-				sortBy,
-				sortOrder,
-				itemLimit,
-				currentPage
-			} = data.needsToBeReorderedTable
+			const header = (
+				<div>
+					<span>Item name</span>
+					<span>Amount</span>
+				</div>
+			)
+
+			const renderItem = item => (
+				<div>
+					<span>{item.name}</span>
+					<span>{item.amount}</span>
+				</div>
+			)
 
 			return(
-				<SortableTableContainer
-					columns={columns}
-					tableRows={tableRows}
-					sortBy={sortBy}
-					sortOrder={sortOrder}
-					sortOrderMutation={UPDATE_NEEDS_TO_BE_REORDERED_TABLE_SORT}
-					itemsPerPage={itemLimit}
-					itemsPerPageMutation={UPDATE_NEEDS_TO_BE_REORDERED_TABLE_ITEM_LIMIT}
-					currentPage={currentPage}
-					pageNumberMutation={UPDATE_NEEDS_TO_BE_REORDERED_TABLE_PAGE_NUMBER}
-					itemsPerPageOptions={[5]}
-					modalMutation={UPDATE_MODAL}
-					entityType=""
+				<ScrollableList
+					header={header}
+					data={dataItems}
+					renderItem={renderItem}
 				/>
 			)
 
