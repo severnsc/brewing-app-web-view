54081994ab7fc0e7b97d60c9e4dda176cae6c37f
Christopher Severns <chris@g2i.co>
2018-10-23 15:51:10 -0400

    Style the profile screen

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -2,8 +2,16 @@ import React from "react"
 import SettingsFormContainer from "../containers/common/settingsFormContainer"
 import ProfileContainer from "../containers/profileContainer"
 
+const styles = {
+	container: {
+		padding: "10px 5%",
+		maxWidth: "600px",
+		alignSelf: "center"
+	}
+}
+
 const Profile = () => (
-	<div>
+	<div style={styles.container}>
 		<ProfileContainer />
 		<SettingsFormContainer isCreating={false} />
 	</div>
