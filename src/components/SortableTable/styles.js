575fa6b530609ff4776a10d2c8bb1b196d0918c7
Christopher Severns <chris@g2i.co>
2018-11-06 09:57:59 -0500

    Remove unused code

--- src/components/SortableTable/styles.js
+++ src/components/SortableTable/styles.js
@@ -1,9 +1,4 @@
-const styles = {
-	pageButton: {
-		textDecoration: "underline",
-    cursor:"pointer",
-    width: "86.73px"
-	},
+export default {
 	container: {
 		display: "flex",
 		"flexFlow": "column"
@@ -13,39 +8,4 @@ const styles = {
 		borderCollapse: "collapse",
 		textAlign: "left"
 	},
-	tableHead: {
-		backgroundColor: "hsl(0, 0%, 95%)",
-		color: "hsl(0, 0%, 50%)",
-		cursor: "pointer"
-	},
-	tableHeadCell: {
-		padding: "20px 10px"
-	},
-	tableData: {
-		borderBottom: "1px solid hsl(0, 0%, 90%)",
-		padding: "20px 10px",
-		color: "hsl(0, 0%, 22%)"
-	},
-	footer: {
-		display: "flex",
-		margin:"0 10px"
-	},
-	span: {
-		flex: "1"
-	},
-	itemsPerPage: {
-		marginLeft: "5px"
-	}
 }
-
-styles.leftPageButton = {
-	...styles.pageButton,
-	textAlign: "left"
-}
-
-styles.rightPageButton = {
-	...styles.pageButton,
-	textAlign: "right"
-}
-
-export default styles
