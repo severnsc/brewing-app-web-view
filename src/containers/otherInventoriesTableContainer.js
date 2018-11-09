6fc105e2eb383bceed5ae240ab60accf47a148b1
Christopher Severns <chris@g2i.co>
2018-11-09 16:05:14 -0500

    Refactor other inventories table container

--- src/containers/otherInventoriesTableContainer.js
+++ src/containers/otherInventoriesTableContainer.js
@@ -1,16 +1,72 @@
 import React from "react"
-import { Query } from "react-apollo"
-import { otherInventoriesTableQuery } from "../queries"
-import shortid from "shortid"
+import { UPDATE_MODAL } from "../mutations"
+import { TableData } from "../components"
 import {
-  UPDATE_OTHER_INVENTORIES_TABLE_SORT,
-  UPDATE_OTHER_INVENTORIES_TABLE_ITEM_LIMIT,
-  UPDATE_OTHER_INVENTORIES_TABLE_PAGE_NUMBER,
-  UPDATE_MODAL
-} from "../mutations"
+	HoverableTableRowContainer,
+	InventoryTableContainer
+} from "./common"
 
-const OtherInventoriesTableContainer = () => (
-	<div></div>
-)
+const OtherInventoriesTableContainer = () => {
+
+	const columns = [
+		"Item name",
+		"Amount"
+	]
+
+	const map = item => ({
+		id: item.id,
+		name: JSON.parse(item.object).name,
+		currentQuantity: item.currentQuantity,
+		quantityUnit: item.quantityUnit
+	})
+
+	const sort = sortBy => 
+		(a, b) => {
+
+			let sortKey
+			switch(sortBy){
+				
+				case "Amount":
+					sortKey = "currentQuantity"
+					break
+
+				default:
+					sortKey = "name"
+			}
+
+			if(a[sortKey] < b[sortKey]){
+				return -1
+			}
+
+			if(a[sortKey] > b[sortKey]){
+				return 1
+			}
+
+			return 0
+		}
+
+	const render = item => (
+		<HoverableTableRowContainer
+			key={item.id}
+			modalMutation={UPDATE_MODAL}
+			entityType="other"
+			id={item.id}
+		>
+			<TableData value={item.name} />
+			<TableData value={item.currentQuantity} />
+		</HoverableTableRowContainer>
+	)
+	
+	return(
+		<InventoryTableContainer
+			name="other"
+			columns={columns}
+			itemsPerPageOptions={[5, 10, 25, 50]}
+			map={map}
+			sort={sort}
+			render={render}
+		/>
+	)
+}
 
 export default OtherInventoriesTableContainer
