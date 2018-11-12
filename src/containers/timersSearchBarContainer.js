bf32022376c6957624b8ebf8a3f9a95ad8f6f50d
Christopher Severns <chris@g2i.co>
2018-11-12 12:26:02 -0500

    Initialize value with filterString state

--- src/containers/timersSearchBarContainer.js
+++ src/containers/timersSearchBarContainer.js
@@ -1,13 +1,32 @@
 import React from "react"
+import { Query } from "react-apollo"
+import { tableQuery } from "../queries"
 import SearchBarContainer from "./common/searchBarContainer"
 import { UPDATE_TABLE_FILTER } from "../mutations"
 
-const TimerSearchBarContainer = () => (
-	<SearchBarContainer
-		mutation={UPDATE_TABLE_FILTER}
-		placeholder="Search for timers by name"
-		name="timers"
-	/>
+const TimersTableSearchBarContainer = () => (
+	<Query query={tableQuery} variables={{name: "timers"}}>
+		{({ loading, error, data }) => {
+
+			if(loading) return <p>Loading...</p>
+			if(error){
+				console.error(error)
+				return <p>Error!</p>
+			}
+
+			const { filterString } = data.table
+
+			return(
+				<SearchBarContainer
+					mutation={UPDATE_TABLE_FILTER}
+					placeholder="Search by timer name"
+					name="timers"
+					initialValue={filterString}
+				/>
+			)
+
+		}}
+	</Query>
 )
 
-export default TimerSearchBarContainer
+export default TimersTableSearchBarContainer
